a82d570a932ad67752709b1f47b8fab9ee29b5ec
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-28 21:24:34 -0500

    add start and end times

--- lib/creature.rb
+++ lib/creature.rb
@@ -1,5 +1,5 @@
 class Creature
-  attr_accessor :name, :url, :image, :price, :location, :time, :months
+  attr_accessor :name, :url, :image, :price, :location, :time, :months, :start_time, :end_time
 
   def self.create(creature_hash)
     creature = self.new(creature_hash)
@@ -11,7 +11,22 @@ class Creature
   end
 
   def time=(time)
-    # TODO time instance
+    times = []
+    if time == "All day"
+      times = [0, 23]
+    else
+      times = time.split(" - ").collect do |t|
+      hour, meridiem = t.split(" ")
+      if meridiem == "PM"
+        hour = hour.to_i + 12
+      end
+      hour.to_s
+    end
+    end
+
+    @start_time = times.first
+    @end_time = times.last
+    @time = time
   end
 
   def year=(months)
