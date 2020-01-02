82e29d7b321cc470d931fa64aca6048fb8cb632e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-02 15:25:20 -0500

    comment out Bundler.require for now
    it's not auto requiring all the gems like I think it's supposed to

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
-require 'bundler'
-Bundler.require
-
+# require 'bundler'
+# Bundler.require
 require 'pry'
 require 'colorize'
 require 'nokogiri'
 require 'open-uri'
+require 'require_all'
 
 require './lib/creature'
 require_all 'lib'
