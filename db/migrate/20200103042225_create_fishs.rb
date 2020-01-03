696ffd39957088f8452beed665c560824ab1595a
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-03 14:33:17 -0500

    create table migrations

---
+++ db/migrate/20200103042225_create_fishs.rb
@@ -0,0 +1,26 @@
+class CreateFishs < ActiveRecord::Migration[5.2]
+  def change
+     create_table :fishs do |t|
+      t.string :name
+      t.string :url
+      t.string :image_url
+      t.string :price
+      t.string :location
+      t.string :time
+      t.string :start_time
+      t.string :end_time
+      t.boolean :january
+      t.boolean :february
+      t.boolean :march
+      t.boolean :april
+      t.boolean :may
+      t.boolean :june
+      t.boolean :july
+      t.boolean :august
+      t.boolean :september
+      t.boolean :october
+      t.boolean :november
+      t.boolean :december
+    end
+  end
+end
