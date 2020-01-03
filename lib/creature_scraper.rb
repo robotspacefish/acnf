565995ee67cec82c85bc473bb134a733d0720c43
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-03 16:14:32 -0500

    fix: separate all creature scrapes
    fish and deep sea have shadow sizes and deep sea doesn't have location

--- lib/creature_scraper.rb
+++ lib/creature_scraper.rb
@@ -10,13 +10,13 @@ class CreatureScraper
       creature = nil
       case @type
       when :bug
-        creature = Bug.create(scrape_row(row_doc))
+        creature = Bug.create(scrape_bug_row(row_doc))
       when :fish
-        creature = Fish.create(scrape_row(row_doc))
+        creature = Fish.create(scrape_fish_row(row_doc))
       when :deep_sea
-       creature = DeepSea.create(scrape_row(row_doc))
+       creature = DeepSea.create(scrape_deep_sea_row(row_doc))
       end
-      creature.save
+      # creature.save
     end
   end
 
@@ -26,7 +26,54 @@ class CreatureScraper
       @rows ||= table.css("tr").collect { |row| row.css("td") }.filter { |array| !array.empty? }
     end
 
-    def scrape_row(row)
+    def scrape_fish_row(row)
+      {
+        :name => row[0].text.lstrip.gsub("\n", ""),
+        :url => row[0].css("a").attribute("href").value,
+        :image_url => row[1].css("img").attribute("src").value,
+        :price => row[2].text.lstrip.gsub("\n", ""),
+        :location => row[3].text.lstrip.gsub("\n", ""),
+        :shadow_size => row[4].text.lstrip.gsub("\n", ""),
+        :time => row[5].text.lstrip.gsub("\n", ""),
+        january: row[6].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        february: row[7].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        march: row[8].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        april: row[9].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        may: row[10].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        june: row[11].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        july: row[12].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        august: row[13].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        september: row[14].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        october: row[15].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        november: row[16].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        december: row[17].text.lstrip.gsub("\n", "") == "-" ? false : true
+      }
+    end
+
+    def scrape_deep_sea_row(row)
+      {
+        :name => row[0].text.lstrip.gsub("\n", ""),
+        :url => row[0].css("a").attribute("href").value,
+        :image_url => row[1].css("img").attribute("src").value,
+        :price => row[2].text.lstrip.gsub("\n", ""),
+        :shadow_size => row[3].text.lstrip.gsub("\n", ""),
+        :time => row[4].text.lstrip.gsub("\n", ""),
+        january: row[5].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        february: row[6].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        march: row[7].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        april: row[8].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        may: row[9].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        june: row[10].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        july: row[11].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        august: row[12].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        september: row[13].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        october: row[14].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        november: row[15].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        december: row[16].text.lstrip.gsub("\n", "") == "-" ? false : true
+      }
+    end
+
+    def scrape_bug_row(row)
       {
         :name => row[0].text.lstrip.gsub("\n", ""),
         :url => row[0].css("a").attribute("href").value,
