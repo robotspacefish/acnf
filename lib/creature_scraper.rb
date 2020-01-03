bb05f4627e02f0995792a8795828c4c9bcec712a
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-03 14:31:22 -0500

    delete :months & add each month into main hash

--- lib/creature_scraper.rb
+++ lib/creature_scraper.rb
@@ -32,20 +32,18 @@ class CreatureScraper
         :price => row[2].text.lstrip.gsub("\n", ""),
         :location => row[3].text.lstrip.gsub("\n", ""),
         :time => row[4].text.lstrip.gsub("\n", ""),
-        :months => {
-          january: row[5].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          february: row[6].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          march: row[7].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          april: row[8].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          may: row[9].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          june: row[10].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          july: row[11].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          august: row[12].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          september: row[13].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          october: row[14].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          november: row[15].text.lstrip.gsub("\n", "") == "-" ? false : true,
-          december: row[16].text.lstrip.gsub("\n", "") == "-" ? false : true
-        }
+        january: row[5].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        february: row[6].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        march: row[7].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        april: row[8].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        may: row[9].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        june: row[10].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        july: row[11].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        august: row[12].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        september: row[13].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        october: row[14].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        november: row[15].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        december: row[16].text.lstrip.gsub("\n", "") == "-" ? false : true
       }
     end
 end
